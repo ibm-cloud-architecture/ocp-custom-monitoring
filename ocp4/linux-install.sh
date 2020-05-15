98133d36e6a09211e835bc5e29e36fec900cf048
Alex Feiszli <feiszli@Alexs-MacBook-Pro.local>
2020-05-15 13:36:35 -0400

    reorg

--- ocp4/linux-install.sh
+++ ocp4/linux-install.sh
@@ -15,11 +15,11 @@ oc expose svc grafana-service -n cloudpak-monitoring
 
 oc get routes
 
-oc get secret prometheus-k8s-htpasswd -n openshift-monitoring -o jsonpath='{.data.auth}{"\n"}' | base64 -D > /tmp/htpasswd-tmp
+oc get secret prometheus-k8s-htpasswd -n openshift-monitoring -o jsonpath='{.data.auth}{"\n"}' | base64 -d > /tmp/htpasswd-tmp
 
 htpasswd -s -b  /tmp/htpasswd-tmp grafana-test topsecret
 
-oc patch secret prometheus-k8s-htpasswd -n openshift-monitoring -p "{\"data\":{\"auth\":\"$(base64 /tmp/htpasswd-tmp)\"}}"
+oc patch secret prometheus-k8s-htpasswd -n openshift-monitoring -p "{\"data\":{\"auth\":\"$(base64 -w0 /tmp/htpasswd-tmp)\"}}"
 
 oc delete pod -l app=prometheus -n openshift-monitoring
 
