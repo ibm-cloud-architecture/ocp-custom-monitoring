a744d54c24755b7f65137d30aa0bdc781afb6ebf
Alex Feiszli <feiszli@ibm.com>
2020-06-18 14:29:50 -0400

    removing extra files

--- ocp4/tempscript.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-  oc get secret prometheus-k8s-htpasswd -n openshift-monitoring -o jsonpath='{.data.auth}{"\n"}' | base64 -D > /tmp/htpasswd-tmp
-  oldpass=$(cat /tmp/htpasswd-tmp)
-  addpass='grafana-test:{SHA}EiAf5eICiDvUX8l+hzZuoFGD4OQ='
-  newpass="${oldpass}\n${addpass}"
-  newpass_enc=$(echo -e $newpass | base64)
-  oc patch secret prometheus-k8s-htpasswd -n openshift-monitoring -p "{\"data\":{\"auth\":\"$newpass_enc\"}}"
